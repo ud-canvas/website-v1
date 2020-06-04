556fb8908a4274604030548ee37a35b9686231c7
vighnesh153 <vighnesh.raut13@gmail.com>
2020-06-04 15:04:14 +0530

    add the route navigator button

--- src/app/components/route-nav-btn/route-nav-btn.component.ts
+++ src/app/components/route-nav-btn/route-nav-btn.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-route-nav-btn',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./route-nav-btn.component.scss']
 })
 export class RouteNavBtnComponent implements OnInit {
+  @Input() prevLink: string = null;
+  @Input() nextLink: string = null;
 
   constructor() { }
 
